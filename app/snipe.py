df2898c1a914b5f66080f3a3367f19bb41353517
Brian McClune <bpmcclune@gmail.com>
2019-10-20 11:41:02 +0000

    implemented snipe route

--- app/snipe.py
+++ app/snipe.py
@@ -1,4 +1,8 @@
+import operator
+
 import flask
+
+from . import lda
 
 snipe = flask.Blueprint('snipe', __name__)
 
@@ -6,5 +10,18 @@ snipe = flask.Blueprint('snipe', __name__)
 @snipe.route('/snipe')
 def fire():
     if flask.request.args:
-        return flask.jsonify(flask.request.args)
+        id_ = flask.request.args.get('id')
+        db = lda.get_db()
+        cursor = db.execute('select "index" from articles where id = ?', (id_,))
+        index = cursor.fetchone()['index']
+        corpus = lda.get_corpus()
+        sim_index = lda.get_sim_index()
+        sim_index.num_best = flask.request.args.get('num_best', 10)
+        results = sim_index[corpus[index]]
+        return flask.jsonify([
+            {'similarity': sim.item(),
+             **dict(db.execute('select "id", title, author, date '
+                               'from articles where "index" = ?',
+                               (idx.item(),)).fetchone())}
+            for (idx, sim) in results])
     return 'Sniping...'
