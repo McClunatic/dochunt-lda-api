91c32561cc3d184b3d804ff135b07aec5aa5c3a2
Brian McClune <bpmcclune@gmail.com>
2019-11-03 23:25:44 +0000

    added typecast of num_best from str to int

--- app/snipe.py
+++ app/snipe.py
@@ -13,7 +13,7 @@ def fire():
         index = flask.request.args.get('target', type=int)
         corpus = lda.get_corpus()
         sim_index = lda.get_sim_index()
-        sim_index.num_best = flask.request.args.get('num_best', 10)
+        sim_index.num_best = flask.request.args.get('num_best', 10, type=int)
         results = sim_index[corpus[index]]
         return flask.jsonify([
             {'index': idx.item(), 'similarity': sim.item()}
